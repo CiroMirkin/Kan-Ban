0d489676acc5d98b4b2cfe5eb1ca4fbbf5f4c356
C.Mirkin <ciromirkin@gmail.com>
2023-06-18 19:55:59 -0300

    add: column class

---
+++ src/column.ts
@@ -0,0 +1,43 @@
+import { column, columnInformation } from "./columnInterface";
+import { columnView } from "./columnViewInterface";
+import { task } from "./taskInterface";
+import ColumnView from "./columnView";
+
+
+export default class Column implements column {
+      name: string;
+      id: string;
+      listOftask: Array<task>;
+      columnView: columnView;
+      constructor(name: string, id: string) {
+            this.name = name;
+            this.id = id
+            this.listOftask = []
+            this.columnView = new ColumnView()
+      }
+      getColumnInformation(): columnInformation {
+            return {
+                  id: this.id,
+                  name: this.name
+            }
+      }
+      addNewTask(newTask: task): any {
+            this.listOftask.push(newTask)
+      }
+      deleteTask(taskId: string): any {
+            this.listOftask = this.listOftask.filter((task: task) => 
+                  task.getTaskInformation().id !== taskId
+            )
+      }
+      getTaskForMoveIt(taskId: string): task {
+            const task = this.listOftask.filter((task: task) => 
+                  task.getTaskInformation().id == taskId
+            )[0]
+            return task
+
+      }
+      getColumnElementForShowIt(): HTMLElement {
+            const column = this.columnView.getColumnForShowIt(this.listOftask)
+            return column
+      }
+}
