0d3111980b0c3e41d318867cd980afea3552fcd4
C.Mirkin <ciromirkin@gmail.com>
2023-07-16 17:56:55 -0300

    add: function for create the basic table

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,17 @@ import Column from './column/column';
 
 const managerOfTable = getManagerOfTableInstance()
 
-const column1 = new Column('Tareas pendientes', '1');
-const column2 = new Column('Tareas en proceso', '2');
-const column3 = new Column('Tareas Terminadas', '3');
-const basicTable = managerOfTable.createATable({
-  tableColumns: [column1, column2, column3],
-  tableName: 'tabla basica'
-})
+const createBasicTable = () => {
+  const column1 = new Column('En Espera', '1');
+  const column2 = new Column('En proceso', '2');
+  const column3 = new Column('Terminadas', '3');
+  const basicTable = managerOfTable.createATable({
+    tableColumns: [column1, column2, column3],
+    tableName: 'tabla basica'
+  })
+  return basicTable
+}
+const basicTable = createBasicTable()
 
 async function loadKanbanBoadPage() {
   loadKanbanBoardPageContent();
