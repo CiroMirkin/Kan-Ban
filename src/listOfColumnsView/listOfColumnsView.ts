4d549b88e870cd628ab33a148087900adf42a83c
C.Mirkin <ciromirkin@gmail.com>
2023-08-20 17:33:19 -0300

    add: listOfColumnsView class and listOfColumnsView.ts file

---
+++ src/listOfColumnsView/listOfColumnsView.ts
@@ -0,0 +1,32 @@
+
+interface columnInListInformationForView {
+    name: string;
+    id: string;
+}
+type listOfColumns = columnInListInformationForView[];
+
+export default class ListOfColumnsView {
+    listOfColumns: listOfColumns;
+    constructor(listOfColumns: listOfColumns) {
+        this.listOfColumns = listOfColumns;
+    }
+    private getHTMLContainerElement(): HTMLElement {
+        return document.querySelector<HTMLElement>('#columns')!;
+    }
+    private getHTMLColumnElement({ name, id }: columnInListInformationForView): HTMLElement {
+        const columnElement: HTMLElement = document.createElement('LI')
+        columnElement.classList.add('columns__column')
+        columnElement.setAttribute('id', id)
+        columnElement.innerText = name
+        return columnElement
+    }
+    show() {
+        const listOfColumnsFragmentElement = document.createDocumentFragment()
+        this.listOfColumns.forEach(column => {
+            const columnElement = this.getHTMLColumnElement(column);
+            listOfColumnsFragmentElement.appendChild(columnElement);
+        })
+        const listOfColumnsHTMLContainer = this.getHTMLContainerElement();
+        listOfColumnsHTMLContainer.appendChild(listOfColumnsFragmentElement);
+    }
+}
