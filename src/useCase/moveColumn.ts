9a549992fb5d9606dc70c00c8f0eb9e6135c582e
c.mirkin <ciromirkin@gmail.com>
2023-10-13 10:49:37 -0300

    add: content of the class MoveColumn

--- src/useCase/moveColumn.ts
+++ src/useCase/moveColumn.ts
@@ -1,22 +1,39 @@
-import { column } from "../model/columnModel/columnInterface";
+
 import { table } from "../model/tableModel/tableInterface";
-import DeleteColumnFromTable from "./deleteColumnFromTable";
 
 interface moveColumn {
     table: table;
     moveOnePlaceToRight(columnId: string): void;
     moveOnePlaceToLeft(columnId: string): void;
 }
-
 export default class MoveColumn implements moveColumn {
     table: table;
     constructor(table: table) {
         this.table = table;
     }
     moveOnePlaceToRight(columnId: string): void {
-        console.log(columnId)
+        const table = this.table;
+        const indexOfTheColumn: number = [...table.orderOfColumns].indexOf(columnId);
+        const indexOfTheNextColumn = (indexOfTheColumn + 1);
+        if(indexOfTheColumn <= table.columns.length) {
+            this.move({ table, indexOfTheColumn, indexOfTheNextColumn });
+        }
     }
     moveOnePlaceToLeft(columnId: string): void {
-        console.log(columnId)
+        const table = this.table;
+        const indexOfTheColumn: number = [...table.orderOfColumns].indexOf(columnId);
+        const indexOfTheNextColumn = (indexOfTheColumn - 1);
+        if(indexOfTheColumn <= table.columns.length) {
+            this.move({ table, indexOfTheColumn, indexOfTheNextColumn });
+        }
+    }
+    private move({ table, indexOfTheNextColumn, indexOfTheColumn }: { table: table, indexOfTheNextColumn: number, indexOfTheColumn: number }) {
+            const nextColumn = table.columns[indexOfTheNextColumn];
+            const column = table.columns[indexOfTheColumn];
+            table.columns[indexOfTheNextColumn] = column;
+            table.columns[indexOfTheColumn] = nextColumn;
+            const orderOfTheNextColumn = table.orderOfColumns[indexOfTheNextColumn];
+            table.orderOfColumns[indexOfTheNextColumn] = table.orderOfColumns[indexOfTheColumn];
+            table.orderOfColumns[indexOfTheColumn] = orderOfTheNextColumn;
     }
 }
